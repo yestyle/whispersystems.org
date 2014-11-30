cbfe6a880078dfb367637a6ff72c8a77bb173204
lilia <liliakai@gmail.com>
2014-11-29 18:31:52 -0800

    Magic install link
    User agents matching 'Mac OS' will be redirected to the itunes page for
    Signal. This includes all versions of iOS as well as Mac versions of
    desktop browsers. All others will be redirected to the play store page
    for TextSecure.

---
+++ _assets/javascripts/install.js
@@ -0,0 +1,8 @@
+;(function() {
+  'use strict';
+  if (navigator.userAgent.match('Mac OS')) {
+    window.location = "https://itunes.apple.com/us/app/signal-private-messenger/id874139669";
+  } else {
+    window.location = "https://play.google.com/store/apps/details?id=org.thoughtcrime.securesms";
+  }
+})();
